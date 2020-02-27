f2edc954f7de4d23f52788f27b62a6c52946152e
Stephen Jang <stephen.jang@slalom.com>
2020-02-27 15:01:45 -0500

    Add zsh init script

--- zsh/init.sh
+++ zsh/init.sh
@@ -1,9 +1,4 @@
 # !/bin/bash
 
-# install oh my zsh
-if ! hash zsh 2>/dev/null; then
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-fi
-
-# copy zshrc
-cp ./zsh/zshrc ~/.zshrc
+# copy vimrc
+cp ./vim/vimrc ~/.vimrc
